b63109dfe6c9dcca6dfca5941a1394f9b78867d7
Daniel Cercado <nt_dcercado@mecojax.com>
2022-03-17 13:59:07 -0400

    test old test without fibase  info

--- src/main.js
+++ src/main.js
@@ -2,22 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+//import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDxuk5G5VER2VaFnbNZOVyxpcisVBdoO2M",
-    authDomain: "projectvuec.firebaseapp.com",
-    projectId: "projectvuec",
-    storageBucket: "projectvuec.appspot.com",
-    messagingSenderId: "141018903270",
-    appId: "1:141018903270:web:d5140522179719b94d4127"
-};
+//const firebaseConfig = {
+//     apiKey: "AIzaSyDxuk5G5VER2VaFnbNZOVyxpcisVBdoO2M",
+//     authDomain: "projectvuec.firebaseapp.com",
+//     projectId: "projectvuec",
+//     storageBucket: "projectvuec.appspot.com",
+//     messagingSenderId: "141018903270",
+//     appId: "1:141018903270:web:d5140522179719b94d4127"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
 createApp(App).use(router).use(router).mount('#app')
 

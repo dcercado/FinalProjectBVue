73eb85f99f448e2da98944bea0f474d94e5c2bcc
dcercado <dcercado@mecojax.com>
2022-07-27 16:24:02 -0400

    installed axioa

---
+++ src/views/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import {createStore} from 'vuex'
+
+import Auth from '../Warehouse/Auth'
+
+//Vue.use(Vuex)
+
+
+export default  createStore({
+    modules: {
+        Auth
+    },
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+
+})
---
+++ src/views/testsignup.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="vue-template">
+    <form @submit.prevent="userRegistration">
+      <h3>Sign Up</h3>
+      <div class="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          class="form-control form-control-lg"
+          v-model="username"
+        />
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          class="form-control form-control-lg"
+          v-model="useremail"
+        />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          class="form-control form-control-lg"
+          v-model="userpassword"
+        />
+      </div>
+      <div class="form-group">
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          class="form-control form-control-lg"
+          v-model="userpassword"
+        />
+      </div>
+      <button type="submit" class="btn btn-dark btn-lg btn-block">
+        Sign Up
+      </button>
+      <p class="forgot-password text-right">
+        <a :href="'/'">Already Signed Up</a>
+        <!-- <router-link :to="{ name: 'login' }">sign in?</router-link> -->
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return;
+  },
+};
+</script>
